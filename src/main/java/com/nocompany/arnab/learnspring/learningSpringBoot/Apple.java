f576d93b82b776866196e4ba6b8a3ae6b2de1a15
Arnab Pal <arnab.pal.21@aot.edu.in>
2025-07-31 20:16:49 +0530

    try

---
+++ src/main/java/com/nocompany/arnab/learnspring/learningSpringBoot/Apple.java
@@ -0,0 +1,11 @@
+package com.nocompany.arnab.learnspring.learningSpringBoot;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Apple {
+
+    void eatApple(){
+        System.out.println("Apple eating...");
+    }
+}
